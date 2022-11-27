dbcb3a1e306a2f25b20649022ab11735951097cb
Pulido1307 <52903296+Pulido1307@users.noreply.github.com>
2022-11-27 01:47:17 -0600

    [ADD]Marcadores

--- app/src/main/java/com/polarinsdustries/mapsexamplemp/MainActivity.kt
+++ app/src/main/java/com/polarinsdustries/mapsexamplemp/MainActivity.kt
@@ -2,15 +2,20 @@ package com.polarinsdustries.mapsexamplemp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.polarinsdustries.mapsexamplemp.models.Marcadores
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
@@ -20,8 +25,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
         database = Firebase.database.getReference("Maps")
-
         createMapFragment()
     }
 
@@ -33,11 +38,32 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        createMarker()
+        //createMarker()
+
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+
+                    for (marcadoresSnapShot in snapshot.children){
+                        val marcadorActual = marcadoresSnapShot.getValue(Marcadores::class.java)
+                        createMarker(marcadorActual!!.lat!!.toDouble(), marcadorActual!!.long!!.toDouble())
+                    }
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity,
+                    "Se cayó más feo que la maquina en la liguilla :c",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
     }
 
-    private fun createMarker() {
-        val favoritePlace = LatLng(19.406934919044126, -102.0428062781975)
+    private fun createMarker(lat: Double, lng: Double) {
+        val favoritePlace = LatLng(lat, lng)
         map.addMarker(MarkerOptions().position(favoritePlace).title("Uruapan"))
         map.animateCamera(
             CameraUpdateFactory.newLatLngZoom(favoritePlace, 18f),
---
+++ app/src/main/java/com/polarinsdustries/mapsexamplemp/models/Marcadores.kt
@@ -0,0 +1,3 @@
+package com.polarinsdustries.mapsexamplemp.models
+
+data class Marcadores(val uid: String? = "Hola", val lat: String? = "0.0", val long: String = "0.0")
